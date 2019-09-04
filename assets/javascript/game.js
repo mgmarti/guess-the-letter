c9fc4ad5acee9bc65a9c332d86156a35c47b72e2
monica <mogimartinez@gmail.com>
2019-09-03 19:12:48 -0500

    cleaned up code + user can view all letters guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
-var guesses = ['a','b','c','d','e','f','g','h','i','j','k','l',
-'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var guesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
 console.log(guesses)
 
 var wins = 0;
@@ -12,49 +13,55 @@ console.log(losses)
 console.log(guessesRemaining)
 console.log(guessedLetters)
 
- // Create variables that hold references to the places in the HTML where we want to display things.
- var directionsText = document.getElementById("directions-text");
- var winsText = document.getElementById("wins-text");
- var lossesText = document.getElementById("losses-text");
- var guessedLettersText = document.getElementById("guessedLetters-text");
- var guessesRemainingText = document.getElementById("guessesRemaining-text");
+// Create variables that hold references to the places in the HTML where we want to display things.
+var directionsText = document.getElementById("directions-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessedLettersText = document.getElementById("guessedLetters-text");
+var guessesRemainingText = document.getElementById("guessesRemaining-text");
 
 // Randomly chooses a choice from the options array. This is the Computer's guess.
 var computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
 console.log(computerGuess)
 
+//Clear array
+function reset() {
+    guessedLetters = [];
+    computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
+}
+
 // This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
-var userGuess = event.key;
+document.onkeyup = function (event) {
+    var userGuess = event.key;
 
-// Determines which key was pressed.
+    guessedLetters.push(userGuess);
 
-console.log(userGuess)
-if (userGuess === computerGuess) {
-    console.log("You won!")
-    wins++;
-    computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
+    // Determines which key was pressed.
+
+    console.log(userGuess)
+    if (userGuess === computerGuess) {
+        console.log("You won!")
+        wins++;
+        reset();
+
+    } else if (userGuess !== computerGuess) {
+        guessesRemaining--;
+        guessedLetters.innerHTML = guessedLetters.join(',');
+    }
+
+    if (guessesRemaining === 0) {
+        losses++
+        reset();
+    }
 
 
-} else if (userGuess !== computerGuess) {
-    guessesRemaining--;
-    guessedLetters.innerHTML = guessedLetters.join(',');
+
+    // Hide the directions
+    directionsText.textContent = "";
+
+    // Display the user and computer guesses, and wins/losses/ties.
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
+    guessesRemainingText.textcontent = "Guesses remaining: " + guessesRemaining;
+    guessedLettersText.textContent = "Your guesses so far: " + guessedLetters;
 }
-
-if (guessesRemaining === 0) {
-    losses++
-    computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
-
-}
-
-
-// Hide the directions
-directionsText.textContent = "";
-
-// Display the user and computer guesses, and wins/losses/ties.
-winsText.textContent = "wins: " + wins;
-lossesText.textContent = "losses: " + losses;
-guessesRemainingText.textcontent = "Guesses remaining: " + guessesRemaining;
-guessedLettersText.textContent = "Your guesses so far: " + userGuess;
-}
-
